6b11af7adf428df8159202da94052d7add8ba5ee
marufhasan566 <marufhasan.iitju47@gmail.com>
2023-08-08 16:23:46 +0600

    update all

--- array_forward_reverse_print.c
+++ array_forward_reverse_print.c
@@ -1,20 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
     }
     // forward printf
-    for(int i=0;i<n;i++){
-        printf("%d ",arr[i]);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
     }
     printf("\n");
     // reverse print
-    for(int i=n-1;i>=0;i--){
-        printf("%d ",arr[i]);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", arr[i]);
     }
     return 0;
 }
--- array_sum.c
+++ array_sum.c
@@ -1,20 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
-    //sum of array element:
+    // sum of array elements:
     int sum = 0;
     for (int i = 0; i < n; i++)
     {
-       sum=sum + arr[i]; 
+        sum = sum + arr[i];
     }
     printf("%d", sum);
-    
+
     return 0;
 }
